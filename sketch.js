96d5d551db47b073453c8ed878b9954854823f6e
ProfNetoReboucas <dreboucas@prof.educacao.sp.gov.br>
2024-05-08 13:39:50 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.2/p5.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.2/addons/p5.sound.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <meta charset="utf-8" />
-
-  </head>
-  <body>
-    <main>
-    </main>
-    <script src="sketch.js"></script>
-  </body>
-</html>
+let palavra;
+function palavraAleatoria() {
+  let palavras = ["Tecnologia", "Javascript", "Programação","Alura","Robótica", "P5JS", "Escola"];
+  return random(palavras);
+}
+  function setup(){
+    createCanvas(600,400);
+    palavra = palavraAleatoria();
+    
+    
+  }
+function inicializaCores(){
+  background("#CDDC39");
+  fill("black");
+  textSize(64);
+  textAlign(CENTER, CENTER);
+}
+function draw(){
+  inicializaCores();
+  let maximo = width;
+  let minimo = 0;
+  let quantidade = map(mouseX, 0, width, 0, palavra.length);
+  if(quantidade < 1){
+    palavra = palavraAleatoria();
+  }
+  let parcial = palavra.substring(0,quantidade);
+  text(parcial,300,200);
+}
